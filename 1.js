d107a4a970cafbe4f905113b7193da7d35e7702b
VissZyCrain <aletuchin@yandex.ru>
2023-02-03 15:27:44 +0300

    js_intro Task

--- 1.js
+++ 1.js
@@ -1,3 +1,3 @@
 Math.round
 let user_input = Number.parseInt(prompt('Введите температуру в градусах Цельсия'));
-alert(`Градусы по Цельсию: ${user_input}\nГрадусы по Фаренгейту: ${Math.round((9 / 5) * user_input + 32)}`)
+alert(`Градусы по Цельсию: ${user_input}\nГрадусы по Фаренгейту: ${Math.floor((9 / 5) * user_input + 32)}`)
